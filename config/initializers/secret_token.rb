6e6ad57ed07ba7a9573b56332b5c8245d635af35
K.C. Dahl <kcdahl2000@gmail.com>
2012-05-03 19:09:12 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lucy::Application.config.secret_token = '1ad7fbec543a1cf76d7be0a6e3867eeaf091efe6a5b936b0b837aba06dfe186620943d48c2bc5bdcf35fbc883724f0b2c9103f4a80ddca8d0cd5169ce177d63c'
